01e986c84f6e1d3158543e9594e7f609e59a126f
mmontoy4 <mmontoya@almata.cat>
2021-04-23 19:07:19 +0200

    creada la classe player, el dinosaure es mou de adalt a baix i la noia =

--- scripts/player.js
+++ scripts/player.js
@@ -2,23 +2,51 @@
 //Animacio mort, victoria i correr.
 class Player {
 
-  constructor(strjugador) {
-    if( strjugador === jugador1){
-      this.spritePlayer = p.createSprite(300,300);
+  constructor(p,strjugador) {
+    this.name=
+    this.points =0;
+    if( strjugador === "Jugador1"){
+      this.spritePlayer = p.createSprite(100,300);
       this.spritePlayer.addAnimation('pum','sprites/noia/Walk1.png','sprites/noia/Walk20.png');
       this.spritePlayer.changeAnimation('pum');
+      this.spritePlayer.immovable = true;
     }
-    else if ( strjugador === jugador2)
+    else if ( strjugador === "Jugador2")
     {
       this.spritePlayer = p.createSprite(900,300);
       this.spritePlayer.addAnimation('pam','sprites/dino/Walk1.png','sprites/dino/Walk10.png');
       this.spritePlayer.changeAnimation('pam');
+      this.spritePlayer.immovable = true;
+      this.spritePlayer.mirrorX(-1);
     }
   }
 
-  posicioInicial
-  moveLeft(p)
-  moveRight
+  posicioInicial(strjugador)
+  {
+
+    if( strjugador === "Jugador1"){
+      this.spritePlayer.position.x =100;
+      this.spritePlayer.position.y =300;
+    }
+    else if ( strjugador === "Jugador2")
+    {
+      this.spritePlayer.position.x =900;
+      this.spritePlayer.position.y =300;
+    }
+
+
+
+  }
+  moveUp()
+  {
+      this.spritePlayer.position.y = this.spritePlayer.position.y-5;
+  }
+
+  moveDown()
+  {
+
+      this.spritePlayer.position.y = this.spritePlayer.position.y+5;
+  }
 
 
 }
--- scripts/sketch.js
+++ scripts/sketch.js
@@ -10,8 +10,8 @@ const s = (p) => {
     pilota = new Ball(p);
 
 
-    //noia = new Player("jugador1");
-    //dino = new Player("jugador2");
+    noia = new Player(p,"Jugador1");
+    dino = new Player(p,"Jugador2");
 
   }
 
@@ -21,21 +21,21 @@ const s = (p) => {
     //pilota.addAnimation('run','sprites/pilota/bola1.png','sprites/pilota/bola10.png');
     //pilota.changeAnimation('run');
     //Noia
-    noia=p.createSprite(300,300);
-    noia.addAnimation('pum','sprites/noia/Walk1.png','sprites/noia/Walk20.png');
+    //noia=p.createSprite(300,300);
+    //noia.addAnimation('pum','sprites/noia/Walk1.png','sprites/noia/Walk20.png');
     //noia.addAnimation('pum','sprites/noia100/Walk1.png','sprites/noia100/Walk20.png');
-    noia.changeAnimation('pum');
+    //noia.changeAnimation('pum');
 
-    dino=p.createSprite(900,300);
-    dino.addAnimation('pam','sprites/dino/Walk1.png','sprites/dino/Walk10.png');
-    dino.changeAnimation('pam');
+  //  dino=p.createSprite(900,300);
+  //  dino.addAnimation('pam','sprites/dino/Walk1.png','sprites/dino/Walk10.png');
+  //  dino.changeAnimation('pam');
     pilota.saqueInicial(p);
 
   }
 
   p.draw = function() {
     p.background(fondo);
-    dino.mirrorX(-1);
+  //  dino.mirrorX(-1);
     p.drawSprites();
 
     //Moviment pilota de tenis
@@ -44,44 +44,46 @@ const s = (p) => {
 
     //Moviment del personatge dino sobre l'eix X de dreta - esquerra.
 
-    if(p.keyIsDown(p.RIGHT_ARROW)){
+/*    if(p.keyIsDown(p.RIGHT_ARROW)){
       dino.mirrorX(+1);
       dino.position.x = dino.position.x+5;
     }
     if(p.keyIsDown(p.LEFT_ARROW)){
       dino.mirrorX(-1);
       dino.position.x = dino.position.x-5;
-    }
+    }*/
 
     //Moviment del personatge dino sobre l'eix Y de adalt - abaix.
 
     if(p.keyIsDown(p.UP_ARROW)){
-      dino.position.y = dino.position.y-5;
+      dino.moveUp();
+      //dino.position.y = dino.position.y-5;
     }
     if(p.keyIsDown(p.DOWN_ARROW)){
-      dino.position.y = dino.position.y+5;
+      dino.moveDown();
+      //dino.position.y = dino.position.y+5;
     }
 
     //Moviment del personatge noia sobre l'eix X de esquerra - dreta.
 
-   if(p.keyDown('a')){
+   /*if(p.keyDown('a')){
       noia.mirrorX(-1);
       noia.position.x = noia.position.x-5;
     }
     if(p.keyDown('d')){
       noia.mirrorX(+1);
       noia.position.x = noia.position.x+5;
-    }
+    }*/
 
     //Moviment del personatge noia sobre l'eix Y de adalt - abaix.
 
     if(p.keyDown('w')){
-
-       noia.position.y = noia.position.y-5;
+       noia.moveUp();
+       //noia.position.y = noia.position.y-5;
      }
      if(p.keyDown('s')){
-
-       noia.position.y = noia.position.y+5;
+       noia.moveDown();
+       //noia.position.y = noia.position.y+5;
      }
 
   }
