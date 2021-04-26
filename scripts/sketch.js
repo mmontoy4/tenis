175bac7003d9466ba058b498b03dbb0a0857f9c0
mmontoy4 <mmontoya@almata.cat>
2021-04-26 20:42:06 +0200

    la bola rebota.

--- scripts/sketch.js
+++ scripts/sketch.js
@@ -3,6 +3,8 @@ const s = (p) => {
   var noia;
   var dino;
   var fondo;
+  var walltop;
+  var wallbottom;
 
   p.preload = function() {
 
@@ -17,43 +19,21 @@ const s = (p) => {
 
   p.setup = function() {
     p.createCanvas(1100, 700);
-    //pilota=p.createSprite(500,500);
-    //pilota.addAnimation('run','sprites/pilota/bola1.png','sprites/pilota/bola10.png');
-    //pilota.changeAnimation('run');
-    //Noia
-    //noia=p.createSprite(300,300);
-    //noia.addAnimation('pum','sprites/noia/Walk1.png','sprites/noia/Walk20.png');
-    //noia.addAnimation('pum','sprites/noia100/Walk1.png','sprites/noia100/Walk20.png');
-    //noia.changeAnimation('pum');
+    p.walltop = p.createSprite(p.width/2, -30/2, p.width, 30);
+    p.walltop.immovable = true;
 
-  //  dino=p.createSprite(900,300);
-  //  dino.addAnimation('pam','sprites/dino/Walk1.png','sprites/dino/Walk10.png');
-  //  dino.changeAnimation('pam');
+    p.wallbottom = p.createSprite(p.width/2, p.height+30/2, p.width, 30);
+    p.wallbottom.immovable = true;
     pilota.saqueInicial(p);
 
   }
 
   p.draw = function() {
     p.background(fondo);
+    pilota.ballSprite.bounce(p.walltop);
+    pilota.ballSprite.bounce(p.wallbottom);
   //  dino.mirrorX(-1);
     p.drawSprites();
-
-    //Moviment pilota de tenis
-
-    //La pilota rebota amb els limits de la pista
-
-    //Moviment del personatge dino sobre l'eix X de dreta - esquerra.
-
-/*    if(p.keyIsDown(p.RIGHT_ARROW)){
-      dino.mirrorX(+1);
-      dino.position.x = dino.position.x+5;
-    }
-    if(p.keyIsDown(p.LEFT_ARROW)){
-      dino.mirrorX(-1);
-      dino.position.x = dino.position.x-5;
-    }*/
-
-    //Moviment del personatge dino sobre l'eix Y de adalt - abaix.
 
     if(p.keyIsDown(p.UP_ARROW)){
       dino.moveUp();
@@ -63,19 +43,6 @@ const s = (p) => {
       dino.moveDown();
       //dino.position.y = dino.position.y+5;
     }
-
-    //Moviment del personatge noia sobre l'eix X de esquerra - dreta.
-
-   /*if(p.keyDown('a')){
-      noia.mirrorX(-1);
-      noia.position.x = noia.position.x-5;
-    }
-    if(p.keyDown('d')){
-      noia.mirrorX(+1);
-      noia.position.x = noia.position.x+5;
-    }*/
-
-    //Moviment del personatge noia sobre l'eix Y de adalt - abaix.
 
     if(p.keyDown('w')){
        noia.moveUp();
--- scripts/tennisgame.js
+++ scripts/tennisgame.js
@@ -1,16 +1,15 @@
-class TennisGame {
-  this.playerOne = new Player("jugador1");
-  this.playerTwo = new Player("jugador2");
-  this.pointsWinGame
-  this.backgroundImage
-  this.backgroundmusic
-  this.numberPlayers
-  this.difficultLevel
-  this.ballGame = new Ball();
-  this.timeGame
+/*class TennisGame {
 
 constructor (numplayers, difficultlevel){
-
+  this.playerOne = new Player("jugador1");
+  this.playerTwo = new Player("jugador2");
+  this.pointsWinGame;
+  this.backgroundImage;
+  this.backgroundmusic;
+  this.numberPlayers;
+  this.difficultLevel;
+  this.ballGame = new Ball();
+  this.timeGame;
 }
 
 comprobarFinJuego();
@@ -21,4 +20,4 @@ reiniciarPunto o saquePunto(strjugador)
 
 
 
-}
+}*/
